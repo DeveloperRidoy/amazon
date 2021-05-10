c1e87c3c6cdfd1729e8297c9edd74ef550acc558
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-05-10 08:59:53 +0600

    imitial commit

--- app.js
+++ app.js
@@ -46,6 +46,13 @@ app.all('*', (req, res, next) => next(new AppError(404, 'Resource not found')));
 // error handling middleware
 app.use(error);
 
+// serve static html pages
+app.use(express.static(`${__dirname}/out`))
+
+app.get("*", (req, res) => {
+  res.sendFile(`${__dirname}/out/index.html`);
+});
+
 // define port
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
