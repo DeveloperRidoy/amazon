9e6a81066b0a7e3fa6ffbf3c37c652d06852b070
DeveloperRidoy <freelancerridoy247@gmail.com>
2021-05-11 12:58:08 +0600

    initial commit

--- api/v1/controllers/checkout.js
+++ api/v1/controllers/checkout.js
@@ -5,35 +5,31 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 // @desc           Create a stripe checkout session
 // @accessibility  Private
 exports.createCheckoutSession = catchAsync(async (req, res, next) => {
-  const { cart } = req.body;
-  const line_items = cart.map((item) => ({
-    price_data: {
-      currency: "usd",
-      product_data: {
-        name: item.product.name,
-        images:
-          process.env.NODE_ENV !== "production"
-            ? ["https://m.media-amazon.com/images/I/71vvXGmdKWL._AC_UY218_.jpg"]
-            : [
-                `${req.secure ? 'https': 'http'}://${req.get('host')}/img/products/${item.product.coverPhoto}`
-              ]
-      },
-      unit_amount: item.product.price.toFixed(2) * 100,
-    },
-    quantity: item.quantity,
-  }));
-  const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items,
-      mode: 'payment',
-      customer_email: req.user.email,
-      success_url: `${req.protocol}://${req.get('host')}/shop?alert=your order has been placed&type=success`,
-      cancel_url: `${req.protocol}://${req.get('host')}/cart?alert=Order cancelled&type=danger`
-  })
+  // const { cart } = req.body;
+  // const line_items = cart.map((item) => ({
+  //   price_data: {
+  //     currency: "usd",
+  //     product_data: {
+  //       name: item.product.name,
+  //       images:[`${req.protocol}://${req.get('host')}/img/products/${item.product.coverPhoto}`]
+  //     },
+  //     unit_amount: item.product.price.toFixed(2) * 100,
+  //   },
+  //   quantity: item.quantity,
+  // }));
+  // const session = await stripe.checkout.sessions.create({
+  //     payment_method_types: ['card'],
+  //     line_items,
+  //     mode: 'payment',
+  //     customer_email: req.user.email,
+  //     success_url: `${req.protocol}://${req.get('host')}/shop?alert=your order has been placed&type=success`,
+  //     cancel_url: `${req.protocol}://${req.get('host')}/cart?alert=Order cancelled&type=danger`
+  // })
   return res.json({
       status: 'success',
       message: 'checkout session initiated',
-      data: {sessionId: session.id}
+    // data: { sessionId: session.id },
+      url: `${req.protocol}://${req.get('host')}/img/products/acer-aspire-5.jpg`
   })
 });
   
